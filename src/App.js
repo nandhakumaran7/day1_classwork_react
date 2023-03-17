a3e4f62911cf14aae8e79d095c503b0808bf95be
nandhakumaran7 <nandhamadhankumar2004@gmail.com>
2023-03-17 15:37:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Let we see the output of JAVASCRIPT</h1>
+       <button onClick={window['alertMsg']}>Click to call msgAlert</button>
       </header>
     </div>
   );
